a9ac8c22deff20388fd822cf0a8d1a151e3dde20
jadhavpra123 <jadhavpra123@gmail.com>
2019-04-14 17:21:50 +0530

    Comment Section add

--- Pr/src/main/java/com/programcreek/helloworld/HelloWorldController.java
+++ Pr/src/main/java/com/programcreek/helloworld/HelloWorldController.java
@@ -8,16 +8,13 @@ import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class HelloWorldController {
-	String message = "Welcome to Spring MVC!";
- 
+	String message = "Welcome to Spring MVC!"; 
 	@RequestMapping("/hello")
-	public ModelAndView showMessage(
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller");
- 
+	public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller"); 
 		ModelAndView mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
 		mv.addObject("name", name);
 		return mv;
-	}
-}
+	}//Show Method End
+}// Controller Class end 
